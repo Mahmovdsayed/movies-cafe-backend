21b51f3b091d529f0b6af8b810741262aa5df6b1
Mahmoud Sayed <138359247+Mahmovdsayed@users.noreply.github.com>
2024-04-19 21:51:54 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ import { config } from 'dotenv'
 
 import { initiateApp } from './src/initiate-app.js'
 config({ path: './config/dev.config.env' })
+import cors from 'cors'
 
-const app = express()
+const corsOptions = {
+  origin: "https://movies-cafe-backend.onrender.com/",
+};
 
+const app = express();
+app.use(cors(corsOptions));
 initiateApp(app, express)
