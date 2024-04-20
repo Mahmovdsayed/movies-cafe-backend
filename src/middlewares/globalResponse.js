2f2ee67be8f5a72f69e31f5154b88e81f4deb3ed
Mahmoud Sayed <138359247+Mahmovdsayed@users.noreply.github.com>
2024-04-21 01:02:32 +0200

    Update globalResponse.js

--- src/middlewares/globalResponse.js
+++ src/middlewares/globalResponse.js
@@ -1,6 +1,7 @@
 export const globalResponse = (err, req, res, next) => {
   if (err) {
     return res.status(500).json({
+      success: false,
       message: "Catch error",
       errorMsg: err.message,
     });
